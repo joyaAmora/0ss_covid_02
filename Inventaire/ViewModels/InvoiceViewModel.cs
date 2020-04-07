1854444083ef768ae77bd7c78d02227b61976454
joyaAmora <jade.phaneuf@gmail.com>
2020-04-07 19:53:05 -0400

    3.2 Modèles

--- Inventaire/ViewModels/InvoiceViewModel.cs
+++ Inventaire/ViewModels/InvoiceViewModel.cs
@@ -1,10 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using app_models;
+using System;
 
 namespace BillingManagement.UI.ViewModels
 {
-    public class InvoiceViewModel
+    public class InvoiceViewModel: BaseViewModel
     {
+        const double txQC = 0.05;
+        const double txCA = 0.09975;
+        
+        private Customer customer;
+        private double subTotal;
+
+        public static int InvoiceId { get; set; }
+        public DateTime CreationDateTime { get; private set; }
+        public Customer Customer {
+
+            get => customer;
+            set
+            {
+                customer = value;
+                OnPropertyChanged();
+            }
+        }
+        public double SubTotal {
+            get => subTotal;
+            set
+            {
+                subTotal = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FedTax));
+                OnPropertyChanged(nameof(ProvTax));
+                OnPropertyChanged(nameof(Total));
+            }
+        }
+        public double FedTax
+        {
+            get { return SubTotal * txCA; }
+        }
+        public double ProvTax 
+        { 
+            get { return SubTotal * txQC; }
+        }
+        public double Total 
+        {
+            get { return SubTotal + FedTax + ProvTax; }
+        }
+
+        public InvoiceViewModel(Customer client)
+        {
+            Customer = client;
+            CreationDateTime = DateTime.Now;
+            InvoiceId++;
+        }
+
+        public InvoiceViewModel()
+        {
+            CreationDateTime = DateTime.Now;
+            InvoiceId++;
+        }
     }
 }
